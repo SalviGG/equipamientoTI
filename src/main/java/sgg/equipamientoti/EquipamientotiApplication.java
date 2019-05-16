509b83e6302b476a587c7ff702601182024873a2
Salvador Gutiérrez <kei.miskatonic@gmail.com>
2019-05-16 11:29:44 -0400

    Commit Inicial

---
+++ src/main/java/sgg/equipamientoti/EquipamientotiApplication.java
@@ -0,0 +1,13 @@
+package sgg.equipamientoti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquipamientotiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EquipamientotiApplication.class, args);
+    }
+
+}
